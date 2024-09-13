2a391557084ac7a87878e231ed2583b367ebdcfb
nirbelelti <nirbelelti@gmail.com>
2024-09-13 11:58:22 +0200

    Add development stage

---
+++ stages/development/main.tf
@@ -0,0 +1,38 @@
+
+provider "opentelekomcloud" {
+  user_name   = var.user_name
+  password    = var.password
+  domain_name = var.domain_name
+  tenant_name = var.tenant_name
+  auth_url    = "https://iam.eu-de.otc.t-systems.com/v3"
+}
+
+terraform {
+  required_providers {
+    opentelekomcloud = {
+      source  = "opentelekomcloud/opentelekomcloud"
+      version = ">=1.35.0"
+    }
+  }
+}
+
+module "bucket" {
+  source = "../../modules/bucket"
+
+  bucket_name           = "var.bucket_name"
+  stage_name            = var.stage_name
+  access_control_list    = var.access_control_list
+  tags                  = var.tags
+  allowed_ip            = var.allowed_ip
+}
+
+
+variable "region" {
+  default = ""
+}
+module "identity_project" { #Mandatory for creating a CCE in OTC
+  source = "../../modules/idetitiy_project"
+  project_name      = var.project_name
+  description = "Production environment for CST app"
+}
+
